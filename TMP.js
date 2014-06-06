a7e7f7ae24a7e7ce6f3ed1e9eb79a5a5b7effd0f
Serban Ghita <serbanghita@gmail.com>
2014-06-06 14:48:05 +0300

    Update TMP.js

--- TMP.js
+++ TMP.js
@@ -1,5 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
-
     console.log('Document has loaded.');
-
 });
+
+var test = document.querySelector(".slide");
+console.log(test.textContent);
+
+var test = document.querySelectorAll(".slides");
+for (var i = 0; i < test.length; i++) {
+    console.log(test[i].innerHTML);
+}
+
+var $btn = document.getElementById('btnUpdate');
+$btn.addEventListener("click", function () {
+    console.log('button was clicked');
+});
+
+var btnMouseOver = function(){ 
+    console.log('mouse over button'); 
+}
+$btn.addEventListener('mouseover', btnMouseOver);
+$btn.removeEventListener('mouseover', btnMouseOver);
