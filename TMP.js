103d506562f3a2a845a874e321a7d91c8f7b2d31
Serban Ghita <serbanghita@gmail.com>
2014-06-06 15:30:15 +0300

    Update TMP.js

--- TMP.js
+++ TMP.js
@@ -20,3 +20,28 @@ var btnMouseOver = function(){
 }
 $btn.addEventListener('mouseover', btnMouseOver);
 $btn.removeEventListener('mouseover', btnMouseOver);
+
+$btn.id
+$btn.classList
+$btn.textContent
+$btn.parentNode
+
+var $notice = document.createElement("div");
+$notice.innerHTML = '<b>Like a bawsz!</b>';
+document.body.appendChild($notice);
+
+var $articles = document.querySelector('.articles'); 
+var $article = document.createElement('article'); 
+$article.innerHTML = 'Article content'; 
+$articles.insertBefore($article);
+
+$test.addEventListener('click', function (e) {
+    e.target.parentNode;
+    e.target.children;
+    e.target.previousElementSibling;
+    e.target.nextElementSibling;
+});
+
+Array.prototype.filter.call($elements, function(elem){
+    return true; // return false;
+});
