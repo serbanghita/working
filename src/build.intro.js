44247a1025a3c6202981816913f169cd3c786bc0
Serban Ghita <sghita@adobe.com>
2017-03-20 14:59:09 +0200

    Reorganized the repo.

--- src/build.intro.js
+++
@@ -1,9 +0,0 @@
-/**
-* working.js - Simple JavaScript framework based on jQuery
-* constructor model, plus a few critical methods.
-* Author: Serban Ghita <serbanghita@gmail.com>
-*/
-
-(function(window, undefined){
-    
-    "use strict";  
