fbfeda0326e6e55117c6a422dcb36cd06819800e
Şerban Ghiţă <serbanghita@gmail.com>
2018-09-21 14:34:56 +0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -19,3 +19,12 @@ echo -e "Waiting for PID of $bin_name ..."
 wait $proc_pid
 echo $proc_pid
 ```
+
+```
+# Loop until selenium server is available
+printf 'Waiting Selenium Server to load\n'
+until $(curl --output /dev/null --silent --head --fail http://localhost:4444/wd/hub); do
+    printf '.'
+    sleep 1
+done
+```
