b40939c188477ff2cf84612de6aee4f2ff661eec
Şerban Ghiţă <serbanghita@gmail.com>
2018-10-08 10:07:03 +0300

    Update node.sh

--- node.sh
+++ node.sh
@@ -2,3 +2,8 @@ tree node_modules/ | wc -l
 
 ls node_modules | wc -l
 du -sh node_modules
+
+
+cat packages.json \
+  | jq -r '.packages[] | [.location, .version] | join(" ")' \
+  | awk 'BEGIN {print "Entries:"} {print "  * " $1 " : " $2}'
