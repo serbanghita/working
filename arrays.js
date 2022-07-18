1cdf607a19c9e05c23e3e1d580360f245c2f26a1
Şerban Ghiţă <serbanghita@gmail.com>
2022-07-18 23:05:40 +0300

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -1,3 +1,4 @@
+// generators
 function* splitInChunks(arr, itemsPerChunk) {
  for (i = 0; i < arr.length; i += itemsPerChunk) {
    yield arr.slice(i, itemsPerChunk);
@@ -6,3 +7,18 @@ function* splitInChunks(arr, itemsPerChunk) {
 
 const b = splitInChunks([1,2,3,4,5,6,7], 2);
 console.log([...b]);
+
+// while
+function splitIntoChunks(arr, noOfChunks) {
+  var result = [];
+  var i = 0;
+  
+  while(i < arr.length) {
+    result.push(arr.slice(i, i + noOfChunks));
+    i += noOfChunks;
+  }
+  
+  return result;
+}
+
+console.log(splitIntoChunks([1,2,3,4,5,6,7], 2));
