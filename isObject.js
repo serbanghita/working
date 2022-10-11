58e2f0692168928aeab05b1e39cce9b9f94df3be
Şerban Ghiţă <serbanghita@gmail.com>
2022-10-11 11:49:38 +0300

    added explanation

--- isObject.js
+++ isObject.js
@@ -1,3 +1,13 @@
 function isObject(o) {
   return Object.prototype.toString.call(o) === '[object Object]';
 }
+
+console.log(isObject(Object.create(null))); // true
+console.log(isObject({})); // true
+console.log(isObject([])); // false
+console.log(isObject(null)); // false
+console.log(isObject()); // false
+console.log(isObject("")); // false
+console.log(isObject(function() {})); // false
+console.log(isObject(1)); // false
+console.log(isObject(NaN)); // false
